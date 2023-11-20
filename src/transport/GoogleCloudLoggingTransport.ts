142cceaae56419c69e7cbcdd05ae3a2d8158a446
Roman <13500086+romanzy313@users.noreply.github.com>
2023-11-20 01:03:17 +0100

    high security sucks

--- src/transport/GoogleCloudLoggingTransport.ts
+++ src/transport/GoogleCloudLoggingTransport.ts
@@ -1,6 +1,6 @@
 import { BaseLogTransport, BaseLogTransportOptions } from '../abstracts';
 import { LogLevel, LogSeverity, StdLog } from '../types';
-
+import { Logging } from '@google-cloud/logging';
 // docs https://cloud.google.com/logging/docs/reference/v2/rest/v2/entries/write
 
 /**
@@ -36,6 +36,12 @@ function formatSeverity(level: number): number {
   return 0;
 }
 
+function formatSeverityLib(input: string): string {
+  if (input == 'warning') return 'WARNING';
+  if (input == 'fatal') return 'CRITICAL';
+  return input.toUpperCase();
+}
+
 // https://cloud.google.com/logging/docs/reference/v2/rest/v2/MonitoredResource
 type MonitoredResource = {
   type: string;
@@ -61,6 +67,7 @@ export type GoogleCloudLoggingTransportOptions = {
 };
 
 export default class GoogleCloudLoggingTransport extends BaseLogTransport {
+  private gLogging: Logging;
   protected googOpts: GoogleCloudLoggingTransportOptions;
   // private API_KEY: string
   // private logName: LogNameValue
@@ -70,6 +77,9 @@ export default class GoogleCloudLoggingTransport extends BaseLogTransport {
     super(options, 1);
 
     this.googOpts = options;
+    this.gLogging = new Logging({
+      projectId: 'bedu2-403220',
+    });
   }
   private valOrEval<T>(vv: any, log: StdLog<any>): T {
     return typeof vv === 'function' ? vv(log) : vv;
@@ -100,25 +110,59 @@ export default class GoogleCloudLoggingTransport extends BaseLogTransport {
       },
     };
   }
-
+  // projects//logs/test
   async send(logs: StdLog<any>[]) {
     try {
-      // auth is wierd https://cloud.google.com/docs/authentication/api-keys#console
-      const res = await fetch(
-        `https://logging.googleapis.com/v2/entries:write`,
-        {
-          method: 'POST',
-          headers: {
-            'X-goog-api-key': this.googOpts.API_KEY,
-            'Content-Type': 'application/json; charset=utf-8',
+      const log = logs[0];
+      const logName = log.meta.name || 'default';
+      const severity = formatSeverityLib(log.severity);
+      const metadata = {
+        severity,
+        resource: {
+          type: 'project',
+          labels: {
+            // project_id: '1065060258809',
+            project_id: 'bedu2-403220',
           },
-          body: JSON.stringify(this.generateBody(logs)),
-        }
-      );
+        },
+      };
 
-      console.log('fetch result', res);
+      const gLog = this.gLogging.log(logName);
+      const json_entry = gLog.entry(metadata, {
+        value: log.value,
+        meta: log.meta,
+      });
 
-      console.log(await res.text());
+      const res = await gLog.write(json_entry);
+
+      console.log('write res', res);
+
+      // auth is wierd https://cloud.google.com/docs/authentication/api-keys#console
+      // const token = await this.gLogging.auth.getAccessToken();
+
+      // if (!token) {
+      //   console.log('bad token', token);
+      //   return;
+      // }
+
+      // console.log('the token is', token);
+
+      // const res = await fetch(
+      //   `https://logging.googleapis.com/v2/entries:write`,
+      //   {
+      //     method: 'POST',
+      //     headers: {
+      //       Authorization: `Bearer: ${token}`,
+      //       'X-goog-api-key': this.googOpts.API_KEY,
+      //       'Content-Type': 'application/json; charset=utf-8',
+      //     },
+      //     body: JSON.stringify(this.generateBody(logs)),
+      //   }
+      // );
+
+      // console.log('fetch result', res);
+
+      // console.log(await res.text());
     } catch (error) {
       console.error('failed to send the log');
       console.error(error);
